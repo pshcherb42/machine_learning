4a28b1e10cc03f22d0afead10d980369f3562fae
pshcherb42 <pshcherb@student.42barcelona.com>
2026-06-06 00:41:53 +0200

    libraries

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import sequential
+from tensorflow.keras.layers import LSTM, Dense, Activation
+from tensorflow.keras.optimizers import RMSprop
+
